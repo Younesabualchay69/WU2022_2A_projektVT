527bec8e7e56ccf6d0e672e5afdc48897a373dc6
Younesabualchay69 <93259343+Younesabualchay69@users.noreply.github.com>
2022-03-29 21:58:16 +0200

    1

--- js/script.js
+++ js/script.js
@@ -1,9 +1,12 @@
 const nav = document.getElementById('nav-links')
 const burger = document.getElementById('burger')
+const logo = document.getElementById('logo')
 
 function toggleMenu() {
     nav.classList.toggle('nav-active')
     burger.classList.toggle('crossed_line')
+    logo.classList.toggle('logo-active')
+
 }
 
 burger.addEventListener('click', toggleMenu)

5174e907cc78991b23c6bb1e3711bc35d99aa075
Younesabualchay69 <93259343+Younesabualchay69@users.noreply.github.com>
2022-05-25 09:11:19 +0200

    1

--- js/script_delsida.js
+++ js/script_delsida.js
@@ -1,12 +1,19 @@
 const nav = document.getElementById('nav-links_delsida')
 const burger = document.getElementById('burger_delsida')
 const logo = document.getElementById('logo')
+const more = document.getElementById('more')
 
 function toggleMenu() {
     nav.classList.toggle('nav-active_delsida')
     burger.classList.toggle('crossed_line')
     logo.classList.toggle('logo-active')
+  
 
+} 
+
+function toggleMenuMore() {
+more.classList.toggle('more-active')
 }
 
-burger.addEventListener('click', toggleMenu)
+burger.addEventListener('click', toggleMenu)    
+more.addEventListener('click', toggleMenuMore)  
